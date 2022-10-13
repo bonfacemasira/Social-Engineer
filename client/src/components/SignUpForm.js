f990191b6033d666cc78a2a56fb5e9b54e9c188f
Bonface Masira <bonfacemasira@gmail.com>
2022-10-13 20:10:20 +0300

    add signup form details

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -2,7 +2,75 @@ import React from 'react';
 
 function SignUpForm() {
     return (
-        <div>SignUp</div>
+        <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Register</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary">
+              Login
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Email</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Contact</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Contact"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>License Number</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="License Number"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Experience</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Experience"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Create Account
+            </button>
+          </div>
+          <p className="text-center mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
+      </form>
+    </div>
     )
 }
 
