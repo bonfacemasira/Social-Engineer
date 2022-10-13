6b93578476dd5e805a417b620dbb2498cfcbd1a5
abdirahmanabdi <abdirahmanabdi.ma@gmail.com>
2022-10-14 01:20:37 +0300

    changed css and imported engineers

--- client/src/components/More.jsx
+++ client/src/components/More.jsx
@@ -3,25 +3,25 @@ import { NavLink } from "react-router-dom"
 const Common = (props) => {
   return (
     <>
-      <section id='header' className='d-flex align-items-center'>
-        <div className='container-fluid'>
+      <section id='header' className='text-left'>
+        <div className='md'>
           <div className='row'>
-            <div className='col-10 mx-auto'>
+            <div className='col-12 mx-auto'>
               <div className='row'>
-                <div className='col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column'>
+                <div className='col-md-6 pt-8 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column'>
                   <h1>
                     {props.name} <strong className='brand-name'>Social-Engineer</strong>
                   </h1>
                   <h2 className='my-3'>Delivering results, reliability, & rock solid dependability.</h2>
-                  <div className='mt-3'>
+                  <div className=''>
                     <NavLink to={props.visit} className='btn btn-primary btn-lg'>
                       {props.btnname}
                     </NavLink>
                   </div>
                 </div>
 
-                <div className='col-lg-6 order-1 order-lg-2 header-img'>
-                  <img src={props.imgsrc} alt='' className='img-fluid animated' />
+                <div className='col-md-4 .header-img }'>
+                  <img src={props.imgsrc} alt='' className='' />
                 </div>
               </div>
             </div>
--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import web from "../components/banner.png"
 import Common from "./More"
-
+import { Engineers } from "./Engineers"
 const Home = () => {
   return (
     <>
       <Common name='Find A Job That
 Matches Your Passion with  ' imgsrc={web} visit='/Login' btnname='Get Started ' />
+<Engineers/>
     </>
   )
 }
