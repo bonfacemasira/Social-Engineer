0b111fc3bb5d3a00d74a5a339920fd748a052b9e
abdirahmanabdi <abdirahmanabdi.ma@gmail.com>
2022-10-13 16:57:49 +0300

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,51 @@
-import React, { Component } from 'react';
-import ResponsiveMenu from 'react-responsive-navbar';
-function Nav() {
-  
-  
-  class Example extends Component {
-    render() {
-      return (
-        <ResponsiveMenu
-          menuOpenButton={<div />}
-          menuCloseButton={<div />}
-          changeMenuOn="500px"
-          largeMenuClassName="large-menu-classname"
-          smallMenuClassName="small-menu-classname"
-          menu={
-            <ul>
-              <li>Item 1</li>
-              <li>Item 2</li>
-              <li>Item 3</li>
-              <li>Item 4</li>
-            </ul>
-          }
-        />
-      );
-    }
-  }
+import React from "react"
+import { NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='container-fluid nav_bg'>
+        <div className='row'>
+          <div className='col-10 mx-auto'>
+            <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+              <div className='container-fluid'>
+                <NavLink className='navbar-brand' to='/'>
+                  Social-Engineer
+                </NavLink>
+                <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
+                  <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                  <ul className='navbar-nav mx-auto mb-2 mb-lg-0'>
+                    <li className='nav-item'>
+                      <NavLink className='nav-link active' exact activeClassName='menu_active' aria-current='page' to='/'>
+                        Home
+                      </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' activeClassName='menu_active' to='/services'>
+                        Services
+                      </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' activeClassName='menu_active' to='/about'>
+                        About
+                      </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                      <NavLink className='nav-link' activeClassName='menu_active' to='/contact'>
+                        Contact
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default Nav;
+export default Navbar
