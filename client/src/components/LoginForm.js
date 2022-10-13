c4c68f6d5d4a61558df48c23ab9f3894b5e6d7ff
Bonface Masira <bonfacemasira@gmail.com>
2022-10-13 19:57:23 +0300

    style login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -9,37 +9,25 @@ function Loginform() {
   }
 
   return (
-    // <form onSubmit={ handleSubmit }>
-    //     <label>Email </label>
-    //     <input />
-    // </form>
     <div className="Auth-form-container">
       <form className="Auth-form" onSubmit={handleSubmit}>
         <div className="Auth-form-content">
-          <h3 className="Auth-form-title">Sign In</h3>
+          <h3 className="Auth-form-title">Login</h3>
           <div className="form-group mt-3">
             <label>Email</label>
-            <input
-              type="email"
-              className="form-control mt-1"
-              placeholder="Email *"
-            />
+            <input type="email" className="form-control mt-1" placeholder="Email *" />
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Password *"
-            />
+            <input type="password" className="form-control mt-1" placeholder="Password *" />
           </div>
           <div className="d-grid gap-2 mt-3">
             <button type="submit" className="btn btn-primary">
-              Submit
+              Login
             </button>
           </div>
           <p className="forgot-password text-right mt-2">
-            Forgot <a href="#">password?</a>
+            Not a Member? <a href="#">Register</a>
           </p>
         </div>
       </form>
