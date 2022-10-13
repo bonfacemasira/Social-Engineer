ec0e7433a205851927fffbe7c5489a0915f51d39
abdirahmanabdi <abdirahmanabdi.ma@gmail.com>
2022-10-13 12:03:24 +0300

    changes

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default function Nav() {
+    return(
+
+    <header className='navbar'>
+        <div className='navbar__title'>Social-Engineer</div>
+<div className="Buttons">
+        <button className='navbar__item'>Log In</button>
+        
+        <button className='button2'>Sign Up</button>
+        </div>
+    </header>
+
+
+    )
+}
+
