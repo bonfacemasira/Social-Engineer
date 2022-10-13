afb6d148b92a1727d552e7c72596a46e97e161ac
abdirahmanabdi <abdirahmanabdi.ma@gmail.com>
2022-10-13 16:57:49 +0300

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import Home from './components/homepage';
-import './App.css';
-import Nav from './components/Navbar';
+// import React from "react"
+// import Home from "./Home"
+// import About from "./About"
+// import Services from "./Services"
+// import Contact from "./Contact"
+import Navbar from "./components/Navbar"
+// import { Switch, Route, Redirect } from "react-router-dom"
 
-
-function App() {
+const App = () => {
   return (
-   <Nav/>
-      
+    <>
+      <Navbar />
+      {/* <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/services' component={Services} />
+        <Route exact path='/contact' component={Contact} />
+        <Redirect to='/'></Redirect>
+      </Switch> */}
+    </>
   )
 }
-
-export default App;
+export default App
