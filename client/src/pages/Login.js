71884ebfc1c3bd36786c1ffeb3f0312467954655
Bonface Masira <bonfacemasira@gmail.com>
2022-10-13 19:17:48 +0300

    add login and sign up forms to login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Loginform from '../components/LoginForm';
+import SignUpForm from '../components/SignUpForm';
+
+function Login() {
+    return (
+        <div>
+            <Loginform />
+            <SignUpForm />
+        </div>
+    )
+}
+
+export default Login;
