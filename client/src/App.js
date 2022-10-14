fd64521bd13407b916c3675d79ef7843ab64559c
Bonface Masira <bonfacemasira@gmail.com>
2022-10-14 11:37:19 +0300

    add BroweRouter on the index page and import Login on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
 import Login from './pages/Login';
 
 function App() {
