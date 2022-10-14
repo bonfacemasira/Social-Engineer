e8615a6deab6c0b0e9cceb891e0bf65fa458544d
Bonface Masira <bonfacemasira@gmail.com>
2022-10-14 14:18:17 +0300

    pick all registration values

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function SignUpForm() {
-    return (
-        <div className="Auth-form-container">
-      <form className="Auth-form">
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [contact, setContact] = useState("");
+  const [licenseNumber, setLicenseNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [experience, setExperience] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(password)
+  }
+
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form" onSubmit={handleSubmit}>
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Register</h3>
           <div className="text-center">
-            Already registered?{" "}
-            <span className="link-primary">
-              Login
-            </span>
+            Already registered? <span className="link-primary">Login</span>
           </div>
           <div className="form-group mt-3">
             <label>Full Name</label>
             <input
-              type="email"
+              type="text"
               className="form-control mt-1"
-              placeholder="e.g Jane Doe"
+              placeholder="Jane Doe"
+              value={fullname}
+              onChange={(e) => setFullname(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -25,23 +38,29 @@ function SignUpForm() {
             <input
               type="email"
               className="form-control mt-1"
-              placeholder="Email"
+              placeholder="example@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
             <label>Contact</label>
             <input
-              type="email"
+              type="number"
               className="form-control mt-1"
-              placeholder="Contact"
+              placeholder="2547xxxxxxxxx"
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
             <label>License Number</label>
             <input
-              type="email"
+              type="number"
               className="form-control mt-1"
               placeholder="License Number"
+              value={licenseNumber}
+              onChange={(e) => setLicenseNumber(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -50,14 +69,18 @@ function SignUpForm() {
               type="password"
               className="form-control mt-1"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
             <label>Experience</label>
             <input
-              type="email"
+              type="text"
               className="form-control mt-1"
               placeholder="Experience"
+              value={experience}
+              onChange={(e) => setExperience(e.target.value)}
             />
           </div>
           <div className="d-grid gap-2 mt-3">
@@ -68,7 +91,7 @@ function SignUpForm() {
         </div>
       </form>
     </div>
-    )
+  );
 }
 
 export default SignUpForm;
