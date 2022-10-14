7bff4b2f00691678dd3566d98bd14c55b741461e
abdirahmanabdi <abdirahmanabdi.ma@gmail.com>
2022-10-14 08:46:44 +0300

    Latest Push

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Homepage from "./components/Homepage.jsx"
 import { Signup } from "./components/Signup.jsx"
 import { Login } from "./components/Login.jsx"
+import { Projects } from "./components/Projects.jsx"
 import Navbar from "./components/Navbar"
 import { Switch, Route, Redirect } from "react-router-dom"
 
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path='/' component={Homepage} />
         <Route exact path='/signup' component={Signup}/>
         <Route exact path='/login' component={Login}/>
+        <Route exact path='/projects' component={Projects}/>
        
         <Redirect to='/'></Redirect>
       </Switch>
