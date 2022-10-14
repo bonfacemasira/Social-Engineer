8e9f9b2ce56c68614c8e4173af8dd3ae34bd55cf
Bonface Masira <bonfacemasira@gmail.com>
2022-10-14 11:36:11 +0300

    define routing paths in Login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Loginform from '../components/LoginForm';
+import NavBar from '../components/NavBar';
 import SignUpForm from '../components/SignUpForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from '../components/Layout';
 
 function Login() {
     return (
         <div>
-            <Loginform />
-            <SignUpForm />
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route exact path='/login' element={<Loginform />} />
+                    <Route exact path='/register' element={<SignUpForm />} />
+                    <Route exact path='/' element={<Loginform />} />
+                </Route>
+            </Routes>
         </div>
     )
 }
