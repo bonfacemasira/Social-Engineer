376db0e61ddd6298793ba3ec77dc4734e2f6930b
Bonface Masira <bonfacemasira@gmail.com>
2022-10-14 11:32:46 +0300

    add layout component that has Outlet and Link elements that current selected route

---
+++ client/src/components/Layout.js
@@ -0,0 +1,21 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <h1>Social-Engineer</h1>
+        <button>
+          <Link to="/login">Login</Link>
+        </button>
+        <button>
+          <Link to="/register">Register</Link>
+        </button>
+      </nav>
+
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
