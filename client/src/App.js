d871064e582f117f70e1df595ea18f58e1416014
Bonface Masira <bonfacemasira@gmail.com>
2022-10-14 11:33:39 +0300

    delink navbar and connect only the Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
-import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Login from './pages/Login';
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
   return (
-    <div className="App">
+    <div>
       <Login />
-      {/* <NavBar user = {user} setUser={setUser} />
-      <main>
-        <Routes>
-          <Route path="/login"></Route>
-          <Route path="/register"></Route>
-        </Routes>        
-      </main> */}
     </div>
   );
 }
