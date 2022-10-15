5eb7015fbfaf6fb459c6544fa8606849d4087127
Abdirahman <abdirahmanabdi.ma@gmail.com>
2022-10-15 16:37:33 +0300

    done

--- client/src/components/engineer/Card.jsx
+++ client/src/components/engineer/Card.jsx
@@ -1,23 +1,19 @@
 function Card(props) {
   return (
-    <div className="card mb-3" style={{maxWidth: '540px'}}>
-      <div className="row no-gutters">
-        <div className="col-md-4">
-          {/* <img src="..." className="card-img" alt="..." /> */}
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">{props.engineer.full_name}</h5>
-            <p className="card-text">
-              {props.engineer.license_number}
-            </p>
-            <p className="card-text">
-              <small className="text-muted">{props.engineer.experience}</small>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <section>
+    <div className="carder">
+    <div className="card" >
+  
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Name: {props.engineer.full_name}</li>
+    <li class="list-group-item">Liscence Number: {props.engineer.license_number}</li>
+    <li class="list-group-item">Experience: {props.engineer.experience}</li>
+    
+  </ul>
+</div>
+
+</div>
+</section>
   );
 }
 
